ece86303f3d95f1b340de6d3eb4ba4046709669f
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-07-20 15:31:49 +0200

    Fix user's icon in Login page

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,31 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+
 function Login() {
 
-	return (
-		<main className="main bg-dark">
-			<section className="sign-in-content">
-				<i className="fa fa-user-circle sign-in-icon"></i>
-				<h1>Sign In</h1>
-				<form>
-					<div className="input-wrapper">
-						<label htmlFor="username">Username</label>
-						<input type="text" id="username" />
-					</div>
-					<div className="input-wrapper">
-						<label htmlFor="password">Password</label>
-						<input type="password" id="password" />
-					</div>
-					<div className="input-remember">
-						<input type="checkbox" id="remember-me" />
-						<label htmlFor="remember-me">Remember me</label>
-					</div>
+    return (
+        <main className="main bg-dark">
+            <section className="sign-in-content">
+                <FontAwesomeIcon className="fa fa-user-circle " icon={faUserCircle} />
+                <h1>Sign In</h1>
+                <form>
+                    <div className="input-wrapper">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" />
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" />
+                    </div>
+                    <div className="input-remember">
+                        <input type="checkbox" id="remember-me" />
+                        <label htmlFor="remember-me">Remember me</label>
+                    </div>
 
-					<button type="submit" className="sign-in-button">
-						Sign In
-					</button>
-				</form>
-			</section>
-		</main>
-	);
+                    <button type="submit" className="sign-in-button">
+                        Sign In
+                    </button>
+                </form>
+            </section>
+        </main>
+    );
 }
 
 export default Login;
